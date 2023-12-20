d771e84057ff79aa0328464abe939bf1ee4d6a42
Katherine Chen <katherine.chen@thetradedesk.com>
2023-12-20 12:18:07 +1100

    Add steps to start GCP private operator

---
+++ scripts/prepare_gcp_enclave_metadata.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -ex
+
+ROOT="."
+METADATA_ROOT="$ADMIN_ROOT/src/main/resources/localstack/s3/core"
+OPERATOR_FILE="$METADATA_ROOT/operators/operators.json"
+ENCLAVE_FILE="$METADATA_ROOT/enclaves/enclaves.json"
+
+if [ -z "$OPERATOR_IMAGE_VERSION" ]; then
+  echo "OPERATOR_IMAGE_VERSION can not be empty"
+  exit 1
+fi
+
+# generate enclave id
+enclave_str="V1,true,$OPERATOR_IMAGE_VERSION"
+echo "enclave_str=$enclave_str"
+enclave_id=$(echo -n $enclave_str | openssl dgst -sha256 -binary | openssl base64)
+
+
+# fetch operator key
+OPERATOR_KEY=$(jq -r '.[] | select(.protocol=="gcp-oidc") | .key' $OPERATOR_FILE)
+
+# update gcp-oidc enclave id
+cat <<< $(jq '(.[] | select(.protocol=="gcp-oidc") | .identifier) |='\"$enclave_id\"'' $ENCLAVE_FILE) > $ENCLAVE_FILE
+
+# export to Github output
+echo "OPERATOR_KEY=$OPERATOR_KEY"
+
+if [ -z "$GITHUB_OUTPUT" ]; then
+  echo "not in github action"
+else
+  echo "OPERATOR_KEY=$OPERATOR_KEY" >> $GITHUB_OUTPUT
+fi
