b4be86c83024a694027ab4aec6e64bc1f98fe8f4
Katherine Chen <katherine.chen@thetradedesk.com>
2023-12-20 11:11:51 +1100

    Add core, localstack and optout bore url

--- scripts/setup_bore.sh
+++ scripts/setup_bore.sh
@@ -12,7 +12,34 @@ if ! which bore > /dev/null; then
   bore_cmd="./bore"
 fi
 
-# start and check endpoint
-$bore_cmd local --local-host core --to bore.pub 8088
-$bore_cmd local --local-host optout --to bore.pub 8081
+$bore_cmd local --local-host localhost --to bore.pub 5001  > $ROOT/bore_localhost.out &
+$bore_cmd local --local-host core --to bore.pub 8088  > $ROOT/bore_core.out &
+$bore_cmd local --local-host optout --to bore.pub 8081 > $ROOT/bore_optout.out &
+
+until [ -f $ROOT/bore_localhost.out ] && [ -f $ROOT/bore_core.out ] && [ -f $ROOT/bore_optout.out ]
+do
+  sleep 5
+done
+
+cat $ROOT/bore_localhost.out
+cat $ROOT/bore_core.out
+cat $ROOT/bore_optout.out
+
+BORE_URL_LOCALSTACK=$(cat $ROOT/bore_localhost.out | grep at | cut -d ' ' -f7)
+BORE_URL_CORE=$(cat $ROOT/bore_core.out | grep at | cut -d ' ' -f7)
+BORE_URL_OPTOUT=$(cat $ROOT/bore_optout.out | grep at | cut -d ' ' -f7)
+
+# export to Github output
+echo "BORE_URL_LOCALSTACK=$BORE_URL_LOCALSTACK"
+echo "BORE_URL_CORE=$BORE_URL_CORE"
+echo "BORE_URL_OPTOUT=$BORE_URL_OPTOUT"
+
+if [ -z "$GITHUB_OUTPUT" ]; then
+  echo "not in github action"
+else
+  echo "BORE_URL_LOCALSTACK=$BORE_URL_LOCALSTACK" >> $GITHUB_OUTPUT
+  echo "BORE_URL_CORE=$BORE_URL_CORE" >> $GITHUB_OUTPUT
+  echo "BORE_URL_OPTOUT=$BORE_URL_OPTOUT" >> $GITHUB_OUTPUT
+fi
+
 
