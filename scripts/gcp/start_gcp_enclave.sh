cc61951a16df5abb1c163c8a2748c5deeb1a25d3
Katherine Chen <cykatherinee@gmail.com>
2024-01-09 12:16:29 +1100

    UID2-2339 create test pipeline for gcp (#48)
    * Set up bore
    
    * Add core, localstack and optout bore url
    
    * Add steps to start GCP private operator
    
    * Run docker compose up before starting gcp private operator
    
    * Fix typo for bore localhost
    
    * Replace config files with bore urls
    
    * Change localhost name to localhost for bore
    
    * Add stop gcp instance step
    
    * Test
    
    * Self host a bore server
    
    * Revert jq replacing
    
    * Fix ampersand operator
    
    * Use bore.pub
    
    * Test different steps
    
    * Test spinning up GCP instance
    
    * Use docker to run bore
    
    * Add `http://` prefix
    
    * Rename `prepare_resources_for_e2e_docker_compose.sh`
    
    * Use sha256 hash for operator image
    
    * Login to GCP registry
    
    * Authenticate with Google Cloud first
    
    * Add http protocol for bore.pub
    
    * Separate `prepare_gcp_metadata` and `start_gcp_private_operator`
    
    * Override value for `enforce_https`
    
    * Remove stop gcp instances code from starting script
    
    * Add steps to run E2E tests for private operator
    
    * Do not pass in env var as inputs
    
    * Do not replace base url for public operator
    
    * Create docker network as a step
    
    * Use enum value for cloud provider
    
    * Add condition when stopping gcp operator instances
    
    * Remove public check condition for checking env vars
    
    * Add wrong if statement to test dockerfile change
    
    * Revert test if statement
    
    * Remove required constraint on gcp specific inputs
    
    * Rename jq_helper to meaningful names
    
    * Move gcp specific scripts to a separate folder
    
    * Update version to v2

---
+++ scripts/gcp/start_gcp_enclave.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+set -ex
+
+ROOT="../uid2-shared-actions/scripts"
+GCP_INSTANCE_NAME="ci-test-$RANDOM"
+OPERATOR_KEY_SECRET_NAME=$GCP_INSTANCE_NAME
+
+source "$ROOT/healthcheck.sh"
+
+if [ -z "$GCP_PROJECT" ]; then
+  echo "GCP_PROJECT can not be empty"
+  exit 1
+fi
+
+if [ -z "$SERVICE_ACCOUNT" ]; then
+  echo "SERVICE_ACCOUNT can not be empty"
+  exit 1
+fi
+
+if [ -z "$OPERATOR_KEY" ]; then
+  echo "OPERATOR_KEY can not be empty"
+  exit 1
+fi
+
+if [ -z "$IMAGE_HASH" ]; then
+  echo "IMAGE_HASH can not be empty"
+  exit 1
+fi
+
+if [ -z "$BORE_URL_CORE" ]; then
+  echo "BORE_URL_CORE can not be empty"
+  exit 1
+fi
+
+if [ -z "$BORE_URL_OPTOUT" ]; then
+  echo "BORE_URL_OPTOUT can not be empty"
+  exit 1
+fi
+
+gcloud config set project $GCP_PROJECT
+
+gcloud config set compute/zone asia-southeast1-a
+
+# create secret
+echo -n "$OPERATOR_KEY" | gcloud secrets create $OPERATOR_KEY_SECRET_NAME \
+    --replication-policy="automatic" \
+    --data-file=-
+
+OPERATOR_KEY_SECRET_VERSION=$(gcloud secrets versions describe latest --secret $OPERATOR_KEY_SECRET_NAME --format 'value(name)')
+
+gcloud compute instances create $GCP_INSTANCE_NAME \
+    --confidential-compute \
+    --shielded-secure-boot \
+    --maintenance-policy Terminate \
+    --scopes cloud-platform \
+    --image-project confidential-space-images \
+    --image-family confidential-space-debug \
+    --service-account $SERVICE_ACCOUNT \
+    --metadata ^~^tee-image-reference=us-docker.pkg.dev/uid2-prod-project/iabtechlab/uid2-operator@$IMAGE_HASH~tee-restart-policy=Never~tee-container-log-redirect=true~tee-env-DEPLOYMENT_ENVIRONMENT=integ~tee-env-API_TOKEN_SECRET_NAME=$OPERATOR_KEY_SECRET_VERSION~tee-env-CORE_BASE_URL=http://$BORE_URL_CORE~tee-env-OPTOUT_BASE_URL=http://$BORE_URL_OPTOUT~tee-env-ENFORCE_HTTPS=false
+
+# export to Github output
+echo "GCP_INSTANCE_NAME=$GCP_INSTANCE_NAME"
+
+if [ -z "$GITHUB_OUTPUT" ]; then
+  echo "not in github action"
+else
+  echo "GCP_INSTANCE_NAME=$GCP_INSTANCE_NAME" >> $GITHUB_OUTPUT
+fi
+
+# get public IP
+ip=$(gcloud compute instances describe $GCP_INSTANCE_NAME \
+    --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+
+echo "instance ip: $ip"
+echo "uid2_e2e_pipeline_operator_url=http://$ip:8080" >> $GITHUB_OUTPUT
+
+healthcheck_url="http://$ip:8080/ops/healthcheck"
+
+# health check - for 5 mins
+healthcheck "$healthcheck_url" 60
+
--- scripts/prepare_resources_for_e2e_docker_compose.sh
+++ scripts/prepare_resources_for_e2e_docker_compose.sh
@@ -9,6 +9,8 @@ OPERATOR_CONFIG_FILE_DIR="$ROOT/docker/uid2-operator/conf"
 CORE_RESOURCE_FILE_DIR="$ROOT/docker/uid2-core/src"
 OPTOUT_RESOURCE_FILE_DIR="$ROOT/docker/uid2-optout/src"
 
+source "../uid2-shared-actions/scripts/jq_helper.sh"
+source "../uid2-shared-actions/scripts/healthcheck.sh"
 
 if [ -z "$CORE_ROOT" ]; then
   echo "CORE_ROOT can not be empty"
@@ -41,7 +43,9 @@ cp "$OPTOUT_ROOT/run_tool_local_e2e.sh" "$OPTOUT_CONFIG_FILE_DIR"
 cp -r "$OPTOUT_ROOT/src/main/resources/localstack" "$OPTOUT_RESOURCE_FILE_DIR"
 mkdir -p "$OPERATOR_CONFIG_FILE_DIR"
 cp "$OPERATOR_ROOT/conf/default-config.json" "$OPERATOR_CONFIG_FILE_DIR"
-cp "$OPERATOR_ROOT/conf/local-e2e-docker-$OPERATOR_TYPE-config.json" "$OPERATOR_CONFIG_FILE_DIR/local-e2e-docker-config.json"
+if [ $OPERATOR_TYPE == "public" ]; then
+  cp "$OPERATOR_ROOT/conf/local-e2e-docker-$OPERATOR_TYPE-config.json" "$OPERATOR_CONFIG_FILE_DIR/local-e2e-docker-config.json"
+fi
 
 cp "../uid2-e2e/docker-compose.yml" "$ROOT"
 
@@ -76,9 +80,28 @@ sed -i.bak "s#uid2-core:latest#uid2-core:$CORE_VERSION#g" $DOCKER_COMPOSE_FILE
 sed -i.bak "s#uid2-optout:latest#uid2-optout:$OPTOUT_VERSION#g" $DOCKER_COMPOSE_FILE
 sed -i.bak "s#uid2-operator:latest#uid2-operator:$OPERATOR_VERSION#g" $DOCKER_COMPOSE_FILE
 
+# set provide_private_site_data to false to workaround the private site path
+if [ $OPERATOR_TYPE != "public" ]; then
+  jq_string_update $CORE_CONFIG_FILE aws_s3_endpoint "http://$BORE_URL_LOCALSTACK"
+  jq_string_update $CORE_CONFIG_FILE kms_aws_endpoint "http://$BORE_URL_LOCALSTACK"
+  jq_string_update $CORE_CONFIG_FILE core_public_url "http://$BORE_URL_CORE"
+  jq_string_update $CORE_CONFIG_FILE optout_url "http://$BORE_URL_OPTOUT"
+  jq_number_boolean_update $CORE_CONFIG_FILE provide_private_site_data false
+
+  jq_string_update $OPTOUT_CONFIG_FILE aws_s3_endpoint "http://$BORE_URL_LOCALSTACK"
+  jq_string_update $OPTOUT_CONFIG_FILE partners_metadata_path "http://$BORE_URL_CORE/partners/refresh"
+  jq_string_update $OPTOUT_CONFIG_FILE operators_metadata_path "http://$BORE_URL_CORE/operators/refresh"
+  jq_string_update $OPTOUT_CONFIG_FILE core_attest_url "http://$BORE_URL_CORE/attest"
+  jq_string_update $OPTOUT_CONFIG_FILE core_public_url "http://$BORE_URL_CORE"
+  jq_string_update $OPTOUT_CONFIG_FILE optout_url "http://$BORE_URL_OPTOUT"
+fi
+
 cat $CORE_CONFIG_FILE
 cat $OPTOUT_CONFIG_FILE
-cat $OPERATOR_CONFIG_FILE
+
+if [ $OPERATOR_TYPE == "public" ]; then
+  cat $OPERATOR_CONFIG_FILE
+fi
 cat $DOCKER_COMPOSE_FILE
 
 mkdir -p "$OPTOUT_MOUNT" && chmod 777 "$OPTOUT_MOUNT"
@@ -88,3 +111,4 @@ chmod 777 "$OPTOUT_RESOURCE_FILE_DIR/init-aws.sh"
 docker compose --profile "$OPERATOR_TYPE" -f "$DOCKER_COMPOSE_FILE" up -d
 docker ps -a
 docker network ls
+
