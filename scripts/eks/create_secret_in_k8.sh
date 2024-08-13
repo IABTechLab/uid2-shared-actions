b5173ced2688e07d8a77cf6b0e23ed17fce7eefd
Katherine Chen <katherine.chen@thetradedesk.com>
2024-08-13 15:08:11 +1000

    Add admin_root

--- scripts/eks/create_secret_in_k8.sh
+++ scripts/eks/create_secret_in_k8.sh
@@ -1,6 +1,25 @@
+#!/usr/bin/env bash
+set -ex
+
+if [ -z "${ADMIN_ROOT}" ]; then
+  echo "ADMIN_ROOT can not be empty"
+  exit 1
+fi
+
+if [ -z "${BORE_URL_CORE}" ]; then
+  echo "BORE_URL_CORE can not be empty"
+  exit 1
+fi
+
+if [ -z "${BORE_URL_OPTOUT}" ]; then
+  echo "BORE_URL_OPTOUT can not be empty"
+  exit 1
+fi
+
 source "uid2-shared-actions/scripts/jq_helper.sh"
 
 ENCLAVE_PROTOCOL="aws-nitro"
+METADATA_ROOT="${ADMIN_ROOT}/src/main/resources/localstack/s3/core"
 OPERATOR_FILE="${METADATA_ROOT}/operators/operators.json"
 # Fetch operator key
 OPERATOR_KEY=$(jq -r '.[] | select(.protocol=="'${ENCLAVE_PROTOCOL}'") | .key' ${OPERATOR_FILE})
