20f0900c66c2241c5d1a95c80c3d121df3bad3fd
Katherine Chen <cykatherinee@gmail.com>
2024-08-15 11:52:14 +1000

    UID2-3783 Add steps for eks e2e test (#117)
    * Add steps for eks e2e test
    
    * Ignore .key
    
    * Apply yaml files on eks cluster
    
    * Update condition for bringing up bore
    
    * Update start_eks_operator inputs
    
    * Use kcc-UID2-3783-e2e-test-for-eks for shared-action repo
    
    * Update path for prepare_eks_deployment_files.sh
    
    * Add ls for debugging
    
    * Update path for deployment-files
    
    * Update debugging command
    
    * Update path for deployment files
    
    * Remove unnecessary EKS action
    
    * Add logic to create secret in k8
    
    * Update secret.json path
    
    * Add steps to Set up kubernetes context
    
    * Update value for replacing
    
    * Add admin_root
    
    * Sleep 600 for testing
    
    * Add action to prepare for EKS metadata
    
    * Remove localstack bore url for GCP
    
    * Update proper aws_pcr0
    
    * Add steps for stopping EKS operator
    
    * Update description for EKS operator
    
    * Update custom branch inputs
    
    * Update eks_pcr0 input
    
    * Revert kcc-UID2-3783-e2e-test-for-eks to v3
    
    * Remove test sleep

---
+++ scripts/eks/prepare_eks_deployment_files.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -ex
+
+if [ -z "${OPERATOR_ROOT}" ]; then
+  echo "${OPERATOR_ROOT} can not be empty"
+  exit 1
+fi
+
+if [ -z "${IMAGE_VERSION}" ]; then
+  echo "IMAGE_VERSION can not be empty"
+  exit 1
+fi
+
+ROOT="."
+DEPLOYMENT_FILES_ROOT="${OPERATOR_ROOT}/scripts/aws/eks/deployment_files"
+
+ls -al
+
+IMAGE="ghcr.io/iabtechlab/uid2-operator-eks-uid2:${IMAGE_VERSION}"
+
+sed -i "s#IMAGE_PLACEHOLDER#${IMAGE}#g" "${DEPLOYMENT_FILES_ROOT}/deployment.yaml"
+if [[ $? -ne 0 ]]; then
+  echo "Failed to pre-process deployment file"
+  exit 1
+fi
