cc7279122f1d57bf694c8baebc9b831b5db37147
Katherine Chen <katherine.chen@thetradedesk.com>
2024-10-03 13:20:28 +1000

    Modify deployment file from uid2-operator-deployment repo

--- scripts/eks/prepare_eks_deployment_files.sh
+++ scripts/eks/prepare_eks_deployment_files.sh
@@ -13,13 +13,15 @@ fi
 
 ROOT="."
 DEPLOYMENT_FILES_ROOT="${OPERATOR_ROOT}/scripts/aws/eks/deployment_files"
+DEPLOYMENT_FILE="${DEPLOYMENT_FILES_ROOT}/tets-deployment.yaml"
 
 ls -al
 
 IMAGE="ghcr.io/iabtechlab/uid2-operator-eks-uid2:${IMAGE_VERSION}"
 
-sed -i "s#IMAGE_PLACEHOLDER#${IMAGE}#g" "${DEPLOYMENT_FILES_ROOT}/deployment.yaml"
+sed -i "s#IMAGE_PLACEHOLDER#${IMAGE}#g" "${DEPLOYMENT_FILE}"
 if [[ $? -ne 0 ]]; then
   echo "Failed to pre-process deployment file"
   exit 1
 fi
+cat ${DEPLOYMENT_FILE}
