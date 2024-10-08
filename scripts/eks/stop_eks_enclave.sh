8b1c0a41d6e0586338a8aeec565ff706dfc901a1
Katherine Chen <katherine.chen@thetradedesk.com>
2024-10-08 17:57:41 +1100

    Remove unnecessary debugging message

--- scripts/eks/stop_eks_enclave.sh
+++ scripts/eks/stop_eks_enclave.sh
@@ -4,5 +4,3 @@ if [ -z "${IDENTITY_SCOPE}" ]; then
 fi
 
 kubectl delete namespace ${IDENTITY_SCOPE,,} --ignore-not-found=true
-kubectl delete namespace compute --ignore-not-found=true
-kubectl delete namespace nitro-enclaves --ignore-not-found=true
