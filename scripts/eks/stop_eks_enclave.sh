9df79d5513a658caf22e93f121e2e5db43720b61
Katherine Chen <katherine.chen@thetradedesk.com>
2024-10-08 15:54:55 +1100

    Delete unused namspace

--- scripts/eks/stop_eks_enclave.sh
+++ scripts/eks/stop_eks_enclave.sh
@@ -4,3 +4,5 @@ if [ -z "${IDENTITY_SCOPE}" ]; then
 fi
 
 kubectl delete namespace ${IDENTITY_SCOPE,,} --ignore-not-found=true
+kubectl delete namespace compute --ignore-not-found=true
+kubectl delete namespace nitro-enclaves --ignore-not-found=true
