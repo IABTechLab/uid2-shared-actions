709d5468ee9a1cba9675527be56e62d192bffecf
Katherine Chen <katherine.chen@thetradedesk.com>
2024-12-23 17:21:47 +1100

    Exit when tests_run doesn't have value

--- scripts/compile_java_test_and_verify.sh
+++ scripts/compile_java_test_and_verify.sh
@@ -7,13 +7,13 @@ tests_run=$(cat build.log  | grep "Tests run:" | tail -n 1 | sed 's/.*Tests run:
 echo "DEBUG: tests_run = $tests_run"
 
 if [ -z "$tests_run" ]; then
-echo "WARNING: Could not determine the number of tests run."
-tests_run=0
+    echo "WARNING: Could not determine the number of tests run."
+    exit 1
 fi
 
 if [ "$tests_run" -eq 0 ]; then
-echo "ERROR: No tests were run!"
-exit 1
+    echo "ERROR: No tests were run!"
+    exit 1
 fi
 
 echo "INFO: $tests_run tests were run!"
