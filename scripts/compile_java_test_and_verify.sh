6bac3f5a11be70b6bcc8987b337aa8db3caf6947
Katherine Chen <katherine.chen@thetradedesk.com>
2024-12-23 16:18:13 +1100

    Extract scripts

---
+++ scripts/compile_java_test_and_verify.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+mvn -B $EXTRA_FLAGS clean compile test | tee build.log
+
+tests_run=$(cat build.log  | grep "Tests run:" | tail -n 1 | sed 's/.*Tests run: \([0-9]*\).*/\1/')
+
+echo "DEBUG: tests_run = $tests_run"
+
+if [ -z "$tests_run" ]; then
+echo "WARNING: Could not determine the number of tests run."
+tests_run=0
+fi
+
+if [ "$tests_run" -eq 0 ]; then
+echo "ERROR: No tests were run!"
+exit 1
+fi
+
+echo "INFO: $tests_run tests were run!"
