837cba851e928702d333d8ce5b853f6a7d4a17d8
Katherine Chen <katherine.chen@thetradedesk.com>
2024-12-24 15:50:35 +1100

    Use parameter to pass in values

--- scripts/compile_java_test_and_verify.sh
+++ scripts/compile_java_test_and_verify.sh
@@ -1,6 +1,57 @@
 #!/usr/bin/env bash
 
-mvn -B $EXTRA_FLAGS clean compile test | tee build.log
+# Function to display usage instructions
+usage() {
+  echo "Usage: $0 [-s SETTINGS_FILE] [-D NAME=VALUE]"
+  echo "  -s SETTINGS_FILE    Specify Maven settings file"
+  echo "  -D NAME=VALUE       Set a system property (can be used multiple times)"
+  echo "Any remaining arguments will be passed directly to Maven."
+  exit 1
+}
+
+# Initialize variables
+settings_file=""
+system_properties=()  # Use an array for multiple -D options
+
+# Parse command-line options
+while getopts "s:D:" opt; do
+  case $opt in
+    s)
+      settings_file="-s $OPTARG"
+      ;;
+    D)
+      system_properties+=("-D$OPTARG")
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+  esac
+done
+
+# Shift off the processed options, leaving only the remaining arguments
+shift "$((OPTIND-1))"
+
+# Construct Maven command
+mvn_command="mvn -B ${settings_file}"
+
+# Add system properties to the command
+for prop in "${system_properties[@]}"; do
+  mvn_command+=" ${prop}"
+done
+
+# Append any remaining arguments to the Maven command
+mvn_command+=" $@"
+
+# Add the fixed part of the Maven command
+mvn_command+=" clean compile test"
+
+echo "DEBUG: Executing Maven command: ${mvn_command}"
+${mvn_command} | tee build.log
 
 tests_run=$(cat build.log  | grep "Tests run:" | tail -n 1 | sed 's/.*Tests run: \([0-9]*\).*/\1/')
 
