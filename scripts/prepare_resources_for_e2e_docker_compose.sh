3d0b01bf83c543e0a6232658ba54bfe2c5988447
Aleksandrs Ulme <aleksandrs.ulme@thetradedesk.com>
2025-02-14 17:43:10 +0800

    Back to the original

--- scripts/prepare_resources_for_e2e_docker_compose.sh
+++ scripts/prepare_resources_for_e2e_docker_compose.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x
+set -ex
 
 # Prepare conf files
 
@@ -110,14 +110,6 @@ chmod 777 "${CORE_RESOURCE_FILE_DIR}/init-aws.sh"
 chmod 777 "${OPTOUT_RESOURCE_FILE_DIR}/init-aws.sh"
 
 docker compose --profile "${OPERATOR_TYPE}" -f "${DOCKER_COMPOSE_FILE}" up -d --wait
-containers=$(docker compose ps -q)
-for container in $containers; do
-  status=$(docker inspect --format='{{.State.ExitCode}}' $container)
-  if [ "$status" -ne 0 ]; then
-    echo "Container $container exited with status $status. Logs:"
-    docker logs "$container"
-  fi
-done
 
 docker ps -a
 docker network ls
