4883aad9c3953009c68da2aaa60ea32ccf06621c
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2025-02-27 17:21:48 +0800

    Changed GCP URLs to https

--- scripts/gcp/start_gcp_enclave.sh
+++ scripts/gcp/start_gcp_enclave.sh
@@ -56,7 +56,7 @@ gcloud compute instances create ${GCP_INSTANCE_NAME} \
     --image-project confidential-space-images \
     --image-family confidential-space-debug \
     --service-account $SERVICE_ACCOUNT \
-    --metadata ^~^tee-image-reference=us-docker.pkg.dev/uid2-prod-project/iabtechlab/uid2-operator@${IMAGE_HASH}~tee-restart-policy=Never~tee-container-log-redirect=true~tee-env-SKIP_VALIDATIONS=true~tee-env-DEPLOYMENT_ENVIRONMENT=integ~tee-env-API_TOKEN_SECRET_NAME=${OPERATOR_KEY_SECRET_VERSION}~tee-env-CORE_BASE_URL=http://${BORE_URL_CORE}~tee-env-OPTOUT_BASE_URL=http://${BORE_URL_OPTOUT}
+    --metadata ^~^tee-image-reference=us-docker.pkg.dev/uid2-prod-project/iabtechlab/uid2-operator@${IMAGE_HASH}~tee-restart-policy=Never~tee-container-log-redirect=true~tee-env-SKIP_VALIDATIONS=true~tee-env-DEPLOYMENT_ENVIRONMENT=integ~tee-env-API_TOKEN_SECRET_NAME=${OPERATOR_KEY_SECRET_VERSION}~tee-env-CORE_BASE_URL=https://${BORE_URL_CORE}~tee-env-OPTOUT_BASE_URL=https://${BORE_URL_OPTOUT}
 
 # Export to GitHub output
 echo "GCP_INSTANCE_NAME=${GCP_INSTANCE_NAME}"
