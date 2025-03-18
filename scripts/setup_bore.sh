0a4e0152919ec409e05a5cf635cba7e9b0d029e0
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2025-03-18 17:36:13 +0800

    Added URL selection based on solution/env

--- scripts/setup_bore.sh
+++ scripts/setup_bore.sh
@@ -3,28 +3,42 @@ set -ex
 
 ROOT="."
 
-# docker run --init --rm --network e2e_default ekzhang/bore local --local-host localstack --to ${BORE_URL} --secret ${BORE_SECRET} 5001  > ${ROOT}/bore_localstack.out &
-# docker run --init --rm --network e2e_default ekzhang/bore local --local-host core --to ${BORE_URL} --secret ${BORE_SECRET} 8088  > ${ROOT}/bore_core.out &
-# docker run --init --rm --network e2e_default ekzhang/bore local --local-host optout --to ${BORE_URL} --secret ${BORE_SECRET} 8081  > ${ROOT}/bore_optout.out &
+if [ "${TARGET_ENVIRONMENT}" == "mock" ]; then
+  docker run --init --rm --network e2e_default ekzhang/bore local --local-host localstack --to ${BORE_URL} --secret ${BORE_SECRET} 5001  > ${ROOT}/bore_localstack.out &
+  docker run --init --rm --network e2e_default ekzhang/bore local --local-host core --to ${BORE_URL} --secret ${BORE_SECRET} 8088  > ${ROOT}/bore_core.out &
+  docker run --init --rm --network e2e_default ekzhang/bore local --local-host optout --to ${BORE_URL} --secret ${BORE_SECRET} 8081  > ${ROOT}/bore_optout.out &
 
-# until [ -f ${ROOT}/bore_localstack.out ] && [ -f ${ROOT}/bore_core.out ] && [ -f ${ROOT}/bore_optout.out ]
-# do
-#   sleep 5
-# done
+  until [ -f ${ROOT}/bore_localstack.out ] && [ -f ${ROOT}/bore_core.out ] && [ -f ${ROOT}/bore_optout.out ]
+  do
+    sleep 5
+  done
 
-# cat ${ROOT}/bore_localstack.out
-# cat ${ROOT}/bore_core.out
-# cat ${ROOT}/bore_optout.out
+  cat ${ROOT}/bore_localstack.out
+  cat ${ROOT}/bore_core.out
+  cat ${ROOT}/bore_optout.out
 
-# BORE_URL_LOCALSTACK=$(cat ${ROOT}/bore_localstack.out | grep at | cut -d ' ' -f7)
-# BORE_URL_CORE=$(cat ${ROOT}/bore_core.out | grep at | cut -d ' ' -f7)
-# BORE_URL_OPTOUT=$(cat ${ROOT}/bore_optout.out | grep at | cut -d ' ' -f7)
+  BORE_URL_LOCALSTACK=$(cat ${ROOT}/bore_localstack.out | grep at | cut -d ' ' -f7)
+  BORE_URL_CORE=$(cat ${ROOT}/bore_core.out | grep at | cut -d ' ' -f7)
+  BORE_URL_OPTOUT=$(cat ${ROOT}/bore_optout.out | grep at | cut -d ' ' -f7)
+elif [ "${IDENTITY_SCOPE}" == "UID2" && "${TARGET_ENVIRONMENT}" == "integ" ]; then
+  BORE_URL_LOCALSTACK="NOT_REQUIRED"
+  BORE_URL_CORE="core-integ.uidapi.com"
+  BORE_URL_OPTOUT="optout-integ.uidapi.com"
+elif [ "${IDENTITY_SCOPE}" == "UID2" && "${TARGET_ENVIRONMENT}" == "prod" ]; then
+  BORE_URL_LOCALSTACK="NOT_REQUIRED"
+  BORE_URL_CORE="core-prod.uidapi.com"
+  BORE_URL_OPTOUT="optout-prod.uidapi.com"
+elif [ "${IDENTITY_SCOPE}" == "EUID" && "${TARGET_ENVIRONMENT}" == "integ" ]; then
+  BORE_URL_LOCALSTACK="NOT_REQUIRED"
+  BORE_URL_CORE="core.integ.euid.eu"
+  BORE_URL_OPTOUT="optout.integ.euid.eu"
+elif [ "${IDENTITY_SCOPE}" == "EUID" && "${TARGET_ENVIRONMENT}" == "prod" ]; then
+  BORE_URL_LOCALSTACK="NOT_REQUIRED"
+  BORE_URL_CORE="core.prod.euid.eu"
+  BORE_URL_OPTOUT="optout.prod.euid.eu"
+fi
 
-BORE_URL_LOCALSTACK="NOT_REQUIRED"
-BORE_URL_CORE="core-prod.uidapi.com"
-BORE_URL_OPTOUT="optout-prod.uidapi.com"
-
-# export to Github output
+# Export to Github output
 echo "BORE_URL_LOCALSTACK=${BORE_URL_LOCALSTACK}"
 echo "BORE_URL_CORE=${BORE_URL_CORE}"
 echo "BORE_URL_OPTOUT=${BORE_URL_OPTOUT}"
