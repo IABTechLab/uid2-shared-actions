1a6e40d32d05545f4fff4678355533df20d30a2a
Katherine Chen <katherine.chen@thetradedesk.com>
2026-02-26 09:52:55 +1100

    Fix core/optout bore url bugs

--- scripts/aks/prepare_aks_artifacts.sh
+++ scripts/aks/prepare_aks_artifacts.sh
@@ -118,9 +118,8 @@ else
   sed -i "s#operator-deployment#operator-deployment-${RUN_ID}#g" "${OUTPUT_TEMPLATE_FILE}"
   cat ${OUTPUT_TEMPLATE_FILE}
 
-  if [ ${TARGET_ENVIRONMENT} == "mock" ]; then
-    python3 ${SHARED_ACTIONS_ROOT}/scripts/aks/add_env.py ${OUTPUT_TEMPLATE_FILE} uid2-operator CORE_BASE_URL ${BORE_URL_CORE} OPTOUT_BASE_URL ${BORE_URL_OPTOUT} SKIP_VALIDATIONS true
-  fi
+  # Add bore URLs for connecting to mock core/optout services (used in all E2E test environments)
+  python3 ${SHARED_ACTIONS_ROOT}/scripts/aks/add_env.py ${OUTPUT_TEMPLATE_FILE} uid2-operator CORE_BASE_URL ${BORE_URL_CORE} OPTOUT_BASE_URL ${BORE_URL_OPTOUT} SKIP_VALIDATIONS true
 
   cat ${OUTPUT_TEMPLATE_FILE}
   # --- Finished updating yaml file with resources ---
